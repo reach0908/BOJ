bd824f60468ad0bba86123ceb09fe3c0b457d777
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-08-27 05:36:05 +0900

    add BOJ_10971 using Permutation

---
+++ BOJ_10971_2/BOJ_10971_2/BOJ_10971_2.cpp
@@ -0,0 +1,48 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int node[10][10];
+
+int main()
+{
+    int T;
+    cin >> T;
+	for (int i = 0; i < T; i++)
+	{
+		for (int j = 0; j < T; j++) {
+			cin >> node[i][j];
+		}
+	}
+	int result= 2147483647;
+	vector<int> v(T+1);
+	for (int i = 0; i <= T; i++)
+	{
+		v[i] = i;
+	}
+	do {
+		bool flag = true;
+		int sum = 0;
+		v[T] = v[0];
+		for (int i = 0; i < T; i++)
+		{
+			if (node[v[i]][v[i + 1]] == 0) {
+				flag = false;
+				break;
+			}
+			else
+			{
+				sum += node[v[i]][v[i + 1]];
+			}
+		}
+		if (flag) {
+			result = min(result, sum);
+		}
+	} while (next_permutation(v.begin(), v.end()-1));
+	
+	cout << result;
+}
+
