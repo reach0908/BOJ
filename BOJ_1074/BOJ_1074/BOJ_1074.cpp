13ffa240f0a0ea83240a48f0db1a73f19c267b7e
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-07 20:22:17 +0900

    백준 1074번 문제풀이 - 시간초과

---
+++ BOJ_1074/BOJ_1074/BOJ_1074.cpp
@@ -0,0 +1,52 @@
+﻿
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int N;
+int countNum = 0;
+int x, y;
+bool found = false;
+
+void zSearch(int startX, int endX, int startY, int endY) {
+	if (found) {
+		return;
+	}
+	int midX = (startX + endX) / 2;
+	int midY = (startY + endY) / 2;
+	if (midX % 2 == 1&&midY % 2 == 1)
+	{
+		for (int i = startY; i < endY; i++)
+		{
+			for (int j = startX; j < endX; j++) {
+				if (i == x && j == y)
+				{
+					found = true;
+					return;
+				}
+				countNum++;
+			}
+		}
+		return;
+	}
+	else {
+		zSearch(startX, midX, startY, midY);
+		zSearch(midX, endX, startY, midY);
+		zSearch(startX, midX, midY, endY);
+		zSearch(midX, endX, midY, endY);
+	}
+}
+
+int main()
+{
+	cin >> N >> x >> y;
+	//벡터 초기화
+	int rowcol = pow(2, N);
+	zSearch(0, rowcol, 0, rowcol);
+	cout << countNum;
+	return 0;
+}
+
