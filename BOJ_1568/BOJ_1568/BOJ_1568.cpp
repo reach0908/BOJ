741b40705870e3669eb7290fecb87e5a33d50c4e
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-11 01:55:30 +0900

    백준 1568번 문제 풀이

---
+++ BOJ_1568/BOJ_1568/BOJ_1568.cpp
@@ -0,0 +1,31 @@
+﻿// BOJ_1568.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    int index=1;
+    int num=0;
+    while (t!=0)
+    {
+        if (index>t)
+        {
+            index = 1;
+            continue;
+        }
+        else {
+            t -= index;
+            index++;
+            num++;
+        }
+    }
+    cout << num;
+}
+
