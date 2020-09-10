193e2911460e27765ba7b5adc916a2d56277ad32
reach0908 <wlsghkim0908@gmail.com>
2020-09-10 16:46:09 +0900

    백준 문제풀이 업로드

---
+++ BOJ_2751/BOJ_2751/main.cpp
@@ -0,0 +1,80 @@
+//
+//  main.cpp
+//  BOJ_2751
+//
+//  Created by KJH on 2020/09/10.
+//  Copyright © 2020 KJH. All rights reserved.
+//
+// 우선 머지소트로 풀어보기
+
+
+
+
+#include <iostream>
+#include <vector>
+#define MAX_SIZE 1000000
+using namespace std;
+
+int sorted[MAX_SIZE];
+int T;
+
+void merge(int arr[],int start,int mid,int end){
+    int i=start;
+    int j=mid+1;
+    int k=start;
+    while (i<=mid&&j<=end) {
+        if (arr[i]<arr[j]) {
+            sorted[k] = arr[i];
+            i++;
+        }
+        else{
+            sorted[k] = arr[j];
+            j++;
+        }
+        k++;
+    }
+    if (i>mid) {
+        for (int t=j; t<=end; t++) {
+            sorted[k] = arr[t];
+            k++;
+        }
+    }else{
+        for (int t=i; t<=mid; t++) {
+            sorted[k] = arr[t];
+            k++;
+        }
+    }
+    for (int t=start; t<=end; t++) {
+        arr[t] = sorted[t];
+    }
+}
+
+void mergeSort(int arr[], int start,int end){
+    int mid;
+    if (start<end) {
+        mid = (start+end)/2;
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid+1, end);
+        merge(arr,start,mid,end);
+    }
+}
+
+
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> T;
+    int arr[T];
+    for (int i=0; i<T; i++) {
+        cin >> arr[i];
+    }
+    mergeSort(arr,0,T-1);
+    for(int i=0;i<T;i++){
+        cout << arr[i] << "\n";
+    }
+    return 0;
+}
