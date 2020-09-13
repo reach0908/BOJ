248c879d2429d73fab71afbe2ca99e3abf2bd838
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-13 20:01:55 +0900

    GCD합 문제풀이 / 유클리드 호제법 이용 /  입력과 출력범위 생각하여 결과값의 타입 생각하

---
+++ 00. 다시 풀어야 할 문제들/GCD합/GCD합/GCD합.cpp
@@ -0,0 +1,47 @@
+﻿
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int gcd(int a, int b) {
+	if (a<b)
+	{
+		int temp = a;
+		a = b;
+		b = temp;
+	}
+	while (b!=0)
+	{
+		int n = a % b;
+		a = b;
+		b = n;
+	}
+	return a;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+	for (int i = 0; i < t; i++)
+	{
+		int n;
+		cin >> n;
+		vector<int> v(n);
+		for (int j = 0; j < n; j++)
+		{
+			cin >> v[j];
+		}
+		int result = 0;
+		for (int j= 0; j < n;j++)
+		{
+			for (int k = j+1; k < n; k++)
+			{
+				result += gcd(v[j], v[k]);
+			}
+		}
+		cout << result << "\n";
+	}
+}
+
