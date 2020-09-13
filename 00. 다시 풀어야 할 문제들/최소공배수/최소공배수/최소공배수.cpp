317e435498c4910af3a4731a4120ad50a0b2f942
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-13 19:50:14 +0900

    유클리드 호제법을 이용한 최소공배수

---
+++ 00. 다시 풀어야 할 문제들/최소공배수/최소공배수/최소공배수.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+
+
+using namespace std;
+
+int gcd(int a, int b) {
+	if (a<b)
+	{
+		int temp = a;
+		a = b;
+		b = temp;
+	}
+	while (b != 0) {
+		int n = a % b;
+		a = b;
+		b = n;
+	}
+	return a;
+}
+
+int main()
+{
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		cout << (a * b) / gcd(a, b) << "\n";
+	}
+	return 0;
+
+}
+
