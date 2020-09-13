0de07bbd5c2e770b731812f004df0f695f102468
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-13 19:43:51 +0900

    간단한 수학 문제풀이

---
+++ 00. 다시 풀어야 할 문제들/나머지/나머지/나머지.cpp
@@ -0,0 +1,14 @@
+﻿// 나머지.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A, B, C;
+    cin >> A >> B >> C;
+    cout << (A + B) % C << "\n" << ((A % C) + (B % C)) % C << "\n" << (A*B) % C << "\n" << ((A % C) *(B % C)) % C;
+    return 0;
+}
