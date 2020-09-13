601c35e1788aa7d7f1d49d6ef522fc5f34bcbbbc
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-14 01:28:28 +0900

    백준 소수찾기 문제풀이 / 에라토스테네스의 체 이용

---
+++ 00. 다시 풀어야 할 문제들/소수찾기/소수찾기/소수찾기.cpp
@@ -0,0 +1,42 @@
+﻿// 소수찾기.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <math.h>
+
+
+using namespace std;
+
+bool eratos(int n) {
+    if (n==1)
+    {
+        return false;
+    }
+    int k = (int)sqrt(n);
+    for (int i = 2; i <= k; i++)
+    {
+        if (n%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    int count = 0;
+    for (int i = 0; i <n ; i++)
+    {
+        int temp;
+        cin >> temp;
+        if (eratos(temp))
+        {
+            count++;
+        }
+    }
+    cout << count;
+}
