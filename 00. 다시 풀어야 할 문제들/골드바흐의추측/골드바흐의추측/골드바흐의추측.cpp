cc41c9ffb424faf6bb01fd2aafe3c8f6f950fd6a
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-14 02:01:58 +0900

    골드바흐의 추축 풀이 / 에라토스테네스의 체 이용

---
+++ 00. 다시 풀어야 할 문제들/골드바흐의추측/골드바흐의추측/골드바흐의추측.cpp
@@ -0,0 +1,65 @@
+﻿
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> num;
+
+bool eratos(int n) {
+	if (n==1||n%2==0)
+	{
+		return false;
+	}
+	int k = (int)sqrt(n);
+	for (int i = 3; i <=k; i++)
+	{
+		if (n%i==0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+void goldbach(vector<pair<int, int>> &result, int n,int count) {
+	for (int i = 3; i < n; i++)
+	{
+		if (eratos(i)&&eratos(n-i))
+		{
+			result[count].first = i;
+			result[count].second = n - i;
+			cout << num[count] << " = " << result[count].first << " + " << result[count].second << "\n";
+			return;
+		}
+	}
+	cout << "Goldbach's conjecture is wrong\n";
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	while (1)
+	{
+		int temp;
+		cin >> temp;
+		if (temp==0)
+		{
+			break;
+		}
+		num.push_back(temp);
+	}
+	int k = num.size();
+	vector<pair<int, int>> result;
+	result.resize(k);
+	for (int i = 0; i < k; i++)
+	{
+		goldbach(result, num[i], i);
+		
+	}
+}
+
