d3a04e0473fd4efe0965b538121e228274c5ff07
reach0908 <wlsghkim0908@gmail.com>
2020-09-15 14:48:09 +0900

    업데이트

---
+++ 10. 동적프로그래밍/BOJ_11053/BOJ_11053/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  BOJ_11053
+//
+//  Created by KJH on 2020/09/14.
+//  Copyright © 2020 KJH. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+
+vector<int> v;
+
+void LIS(){
+
+}
+
+int main() {
+    cin >> N;
+    int arr[N];
+    for (int i=0; i<N; i++) {
+        cin >> arr[i];
+    }
+    return 0;
+}
