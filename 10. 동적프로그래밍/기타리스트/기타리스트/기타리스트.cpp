7c882adcbdc25ea044b10b60b8bb6d7cfd5d7be1
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-15 23:04:37 +0900

    동적 프로그래밍 기타리스트 풀이

---
+++ 10. 동적프로그래밍/기타리스트/기타리스트/기타리스트.cpp
@@ -0,0 +1,46 @@
+﻿
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, S, M;
+int arr[100];
+int dp[100];
+int main()
+{
+    cin >> N >> S >> M;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    dp[0] = S;
+    for (int i = 1; i <=N; i++)
+    {
+
+        
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+//
+
+3 5 10
+5 3 7 
+
+5 0 3 10 
+  
+
+
+
+ 10 7 0
