a93fd429295f5e2a4a92e52a580a88dc0dc32cfb
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-18 01:13:14 +0900

    next_permutation을 이용한 브루트포스 문제풀이

---
+++ 00. 다시 풀어야 할 문제들/모든 순열/모든 순열/모든 순열.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> v;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int T;
+	cin >> T;
+	for (int i = 1; i <= T; i++) {
+		v.push_back(i);
+	}
+
+	do
+	{
+		for (int i = 0; i < T; i++) {
+			cout << v[i] << " ";
+		}
+		cout << "\n";
+	} while (next_permutation(v.begin(),v.end()));
+	return 0;
+}
+
---
+++ 00. 다시 풀어야 할 문제들/차이를 최대로/차이를 최대로/차이를 최대로.cpp
@@ -0,0 +1,36 @@
+﻿// 차이를 최대로.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T;
+    cin >> T;
+    vector<int> v(T);
+    for (int i = 0; i < T; i++)
+    {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    int maxN=0;
+    do
+    {
+        int sum = 0;
+        for (int i = 0; i < T-1; i++)
+        {
+            sum += abs(v[i] - v[i + 1]);
+        }
+        maxN = max(maxN, sum);
+    } while (next_permutation(v.begin(),v.end()));
+
+    cout << maxN;
+    return 0;
+}
