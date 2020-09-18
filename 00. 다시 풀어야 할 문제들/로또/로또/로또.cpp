7e981d2e7a6d1cf4cf38ac0aabdaeddcf4ca2573
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-19 03:11:45 +0900

    next_permutation을 이용한 문제풀이

---
+++ 00. 다시 풀어야 할 문제들/로또/로또/로또.cpp
@@ -0,0 +1,50 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while (1)
+	{
+		int N;
+		cin >> N;
+		if (N==0)
+		{
+			break;
+		}
+		vector<int> v(N);
+		for (int i = 0; i < N; i++)
+		{
+			cin >> v[i];
+		}
+		vector<int> check;
+		for (int i = 0; i < 6; i++)
+		{
+			check.push_back(0);
+		}
+		for (int i = 0; i < N-6; i++)
+		{
+			check.push_back(1);
+		}
+		do
+		{
+			for (int i = 0; i < N; i++)
+			{
+				if (check[i]==0)
+				{
+					cout << v[i] << " ";
+				}
+			}
+			cout << "\n";
+		} while (next_permutation(check.begin(),check.end()));
+		cout << "\n";
+	}
+	return 0;
+}
