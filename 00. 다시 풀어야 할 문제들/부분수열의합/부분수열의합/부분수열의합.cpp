609c68ef71a54f9707186a5078f82102193c146a
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-20 05:19:24 +0900

    DFS를 이용한 부분수열의 합 문제풀이

--- 00. 다시 풀어야 할 문제들/부분수열의합/부분수열의합/부분수열의합.cpp
+++ 00. 다시 풀어야 할 문제들/부분수열의합/부분수열의합/부분수열의합.cpp
@@ -5,16 +5,18 @@ using namespace std;
 
 int N,S;
 vector<int> num;
-int result;
-void func(int start,int end,int sum) {
-	for (int i = start; i < end; i++)
-	{
-		sum += num[i];
-	}
-	if (sum==S)
+int result=0;
+
+
+void DFS(int i, int sum) {
+	if (i == N) return;
+	if (sum+num[i]==S)
 	{
 		result++;
 	}
+	DFS(i + 1, sum); // 이번거는 스킵하고 다음 원소부터의 부분수열
+	DFS(i + 1, sum + num[i]); // 이번거를 포함하는 부분수열
+
 }
 
 int main()
@@ -25,13 +27,7 @@ int main()
 	{
 		cin >> num[i];
 	}
-	for (int i = 0; i < N-1; i++)
-	{
-		for (int j = i; j < N; j++)
-		{
-			func(i, j, 0);
-		}
-	}
+	DFS(0, 0);
 	cout << result;
 }
 
