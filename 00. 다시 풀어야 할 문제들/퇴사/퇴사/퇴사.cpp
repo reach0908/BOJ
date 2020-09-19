502d0fe043692fe0b329d0446b335a22487a0121
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-20 05:50:47 +0900

    DFS를 이용한 문제풀이

---
+++ 00. 다시 풀어야 할 문제들/퇴사/퇴사/퇴사.cpp
@@ -0,0 +1,40 @@
+﻿
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int DAY;
+
+vector<pair<int, int>> v;
+
+int MAX = 0;
+
+void func(int start, int value) {
+	if (start>DAY)
+	{
+		return;
+	}
+	if (start + v[start].first <= DAY+1) {
+		MAX = max(MAX, value + v[start].second);
+	}
+	func(start + 1, value);
+	func(start + v[start].first, value + v[start].second);
+}// 소요되는 날짜,가치
+
+int main()
+{
+	cin >> DAY;
+	v.resize(DAY+1);
+	v[0].first = 1;
+	v[0].second = 0;
+	for (int i = 1; i <= DAY; i++)
+	{
+		cin >> v[i].first >> v[i].second;
+	}
+	func(0, 0);
+	cout << MAX;
+}
+
