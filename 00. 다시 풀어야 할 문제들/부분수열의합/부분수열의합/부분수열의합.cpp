0964bb7d9885ae864d5e2c2196b69d0cc8822aea
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-20 03:45:01 +0900

    부분수열의 합

---
+++ 00. 다시 풀어야 할 문제들/부분수열의합/부분수열의합/부분수열의합.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N,S;
+vector<int> num;
+int result;
+void func(int start,int end,int sum) {
+	for (int i = start; i < end; i++)
+	{
+		sum += num[i];
+	}
+	if (sum==S)
+	{
+		result++;
+	}
+}
+
+int main()
+{
+    cin >> N >> S;
+	num.resize(N);
+	for (int i = 0; i < N; i++)
+	{
+		cin >> num[i];
+	}
+	for (int i = 0; i < N-1; i++)
+	{
+		for (int j = i; j < N; j++)
+		{
+			func(i, j, 0);
+		}
+	}
+	cout << result;
+}
+
