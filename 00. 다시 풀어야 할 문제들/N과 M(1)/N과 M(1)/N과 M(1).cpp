86c0c7a6136767b741f2daf5b2d1e7ead4d5bdbe
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-20 23:59:44 +0900

    백트래킹을 이용한 문제풀이

---
+++ 00. 다시 풀어야 할 문제들/N과 M(1)/N과 M(1)/N과 M(1).cpp
@@ -0,0 +1,44 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+int arr[9];
+bool visited[9];
+
+void func(int cnt) {
+	if (cnt == M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = 1; i <=N; i++) //경우의 수에대한 모든 탐색
+	{
+		if (!visited[i])
+		{
+			visited[i] = true;
+			arr[cnt] = i; 
+			func(cnt + 1); 
+			visited[i] = false; //성공하기 전단계로 돌아가기
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+    cin >> N >> M;
+
+	func(0);
+	return 0;
+}
+
