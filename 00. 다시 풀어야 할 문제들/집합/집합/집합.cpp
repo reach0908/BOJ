ec0fe64c1a00d2261118d8b5917ce2449da7e342
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-20 21:47:59 +0900

    비트마스크를 이용한 문제풀이

---
+++ 00. 다시 풀어야 할 문제들/집합/집합/집합.cpp
@@ -0,0 +1,66 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int N;
+bool arr[21];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    cin >> N;
+	fill(arr, arr + 21, false);
+	for (int i = 0; i < N; i++)
+	{
+		string s;
+		cin >> s;
+		
+		if (s=="add")
+		{
+			int index;
+			cin >> index;
+			arr[index] = true;
+		}
+		if (s == "check") {
+			int index;
+			cin >> index;
+			if (arr[index])
+			{
+				cout << 1 << "\n";
+			}
+			else {
+				cout << 0 << "\n";
+			}
+		}
+		if (s=="remove")
+		{
+			int index;
+			cin >> index;
+			arr[index] = false;
+		}
+		if (s == "toggle") {
+			int index;
+			cin >> index;
+			if (arr[index])
+			{
+				arr[index] = false;
+			}
+			else {
+				arr[index] = true;
+			}
+		}
+		if (s=="all") {
+			fill(arr, arr + 21, true);
+		}
+		if (s=="empty") {
+			fill(arr, arr + 21, false);
+		}
+	}
+	return 0;
+}
+
