c85925a4a6964104db0d1619c38ff3e05b094e5e
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-21 23:14:37 +0900

    백트래킹을 이용한 문제풀이

---
+++ 00. 다시 풀어야 할 문제들/N과M(1)/N과M(1)/N과M(1).cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool visited[9] = { false };
+vector<int> v;
+int N, M;
+int arr[9];
+void func(int cnt) {
+	if (cnt==M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << endl;
+	}
+	for (int i = 1; i <= N; i++)
+	{
+		if (!visited[i])
+		{
+			visited[i] = true;
+			arr[cnt] = i;
+			func(cnt + 1);
+			visited[i] = false;
+		}
+	}
+
+}
+int main()
+{
+	cin >> N >> M;
+	func(0);
+	return 0;
+	
+}
+
---
+++ 00. 다시 풀어야 할 문제들/N과M(2)/N과M(2)/N과M(2).cpp
@@ -0,0 +1,41 @@
+﻿// N과M(2).cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+const int MAX = 9;
+using namespace std;
+
+int arr[9] = {0};
+int N, M;
+bool visited[MAX];
+void func(int cnt) {
+    if (cnt == M) {
+        for (int i = 0; i < M; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        return;
+    }
+    for (int i = 1; i <= N; i++)
+    {
+        if (!visited[i]) {
+            if (arr[cnt-1]<i)
+            {
+                visited[i] = true;
+                arr[cnt] = i;
+                func(cnt + 1);
+                visited[i] = false;
+            }
+        }
+    }
+
+}
+
+int main()
+{
+    cin >> N >> M;
+    func(0);
+    return 0;
+}
+
