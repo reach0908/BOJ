2e37ee22c53417c010b46c7a584532c8375a8b5c
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-21 22:44:26 +0900

    탐욕알고리즘을 이용한 문제풀이 / 우선순위 큐를 이용하여 데드라인에 맞게 문제수 조정

---
+++ 13. 탐욕 알고리즘/컵라면/컵라면/컵라면.cpp
@@ -0,0 +1,41 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+priority_queue<int, vector<int>, greater<int>> pq;
+int N;
+vector<pair<int, int>> ramen;
+int main()
+{
+    cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		ramen.push_back(make_pair(a, b));
+	}
+	stable_sort(ramen.begin(), ramen.end());
+	
+	for (int i = 0; i < N; i++)
+	{
+		pq.push(ramen[i].second);
+		if (pq.size()>ramen[i].first)
+		{
+			pq.pop();
+		}
+	}
+	int result = 0;
+	while (!pq.empty())
+	{
+		int temp = pq.top();
+		pq.pop();
+		result += temp;
+	}
+	cout << result;
+	return 0;
+}
+
