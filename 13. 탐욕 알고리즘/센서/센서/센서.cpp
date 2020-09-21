916a3398993c512f0d5327f0388f8557fbafc63d
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-21 11:00:36 +0900

    탐욕알고리즘을 이용한 문제풀이

---
+++ 13. 탐욕 알고리즘/센서/센서/센서.cpp
@@ -0,0 +1,36 @@
+﻿
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+const int MAX = 100001;
+int N, K;
+
+int v[MAX] = { 0 };
+int main()
+{
+	cin >> N >> K;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> v[i];
+	}
+	sort(v, v + N);
+	vector<int> diff(N - 1);
+	for (int i = 0; i < N - 1; i++)
+	{
+		diff[i] = v[i + 1] - v[i];
+	}
+	sort(diff.begin(), diff.end());
+	int result = 0;
+	for (int i = 0; i < N - K; i++)
+	{
+		result += diff[i];
+	}
+	cout << result << "\n";
+	return 0;
+
+
+}
+
