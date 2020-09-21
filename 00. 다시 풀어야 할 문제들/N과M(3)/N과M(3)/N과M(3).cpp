f94b94e5ec627af9f7bf7ba994c524e54c0c845d
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-21 23:21:37 +0900

    백트래킹을 이용한 문제풀이

---
+++ 00. 다시 풀어야 할 문제들/N과M(3)/N과M(3)/N과M(3).cpp
@@ -0,0 +1,36 @@
+﻿
+
+#include <iostream>
+const int MAX = 9;
+using namespace std;
+
+int arr[MAX];
+int N, M;
+
+void func(int cnt) {
+	if (cnt==M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = 1; i <= N; i++)
+	{
+		arr[cnt] = i;
+		func(cnt + 1);
+	}
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+    cin >> N >> M;
+    func(0);
+}
