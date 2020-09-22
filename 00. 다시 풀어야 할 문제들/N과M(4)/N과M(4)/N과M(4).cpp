4f4608c13a1cdd92b5acd59f61f652fd9df0d897
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-22 19:48:25 +0900

    DFS를 이용한 문제풀이

---
+++ 00. 다시 풀어야 할 문제들/N과M(4)/N과M(4)/N과M(4).cpp
@@ -0,0 +1,38 @@
+﻿// N과M(4).cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+const int MAX = 9;
+int N,M;
+int arr[MAX] = {-1};
+void DFS(int cnt) {
+	if (cnt==M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = 1; i <= N; i++)
+	{
+		arr[cnt] = i;
+		if (arr[cnt]>=arr[cnt-1])
+		{
+			DFS(cnt + 1);
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> N>>M;
+	DFS(0);
+}
+
