2abbdac7a78aa94d821f1e8e5d5e2270ef98ab73
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-22 20:05:05 +0900

    DFS를 이용한 문제풀이

---
+++ 00. 다시 풀어야 할 문제들/N과M(6)/N과M(6)/N과M(6).cpp
@@ -0,0 +1,53 @@
+﻿// N과M(6).cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+const int MAX = 9;
+
+using namespace std;
+
+int N, M;
+vector<int> v;
+bool visited[MAX] = { false };
+
+int arr[MAX];
+
+void DFS(int cnt) {
+    if (cnt == M) {
+        for (int i = 0; i < M; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    for (int i = 0; i < N; i++)
+    {
+        if (!visited[i]) {
+
+            visited[i] = true;
+            arr[cnt] = v[i];
+            if (arr[cnt] > arr[cnt - 1]) {
+                DFS(cnt + 1);
+            }
+            visited[i] = false;
+        }
+    }
+}
+
+
+int main()
+{
+    cin >> N >> M;
+    for (int i = 0; i < N; i++)
+    {
+        int temp;
+        cin >> temp;
+        v.push_back(temp);
+    }
+    sort(v.begin(), v.end());
+    DFS(0);
+}
+
