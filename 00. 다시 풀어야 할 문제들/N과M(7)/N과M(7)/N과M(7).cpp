3fd65b8b81f4412cd1d0aa310178b4954169a591
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-24 03:59:49 +0900

    DFS를 이용한 문제풀이

---
+++ 00. 다시 풀어야 할 문제들/N과M(7)/N과M(7)/N과M(7).cpp
@@ -0,0 +1,47 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+
+vector<int> v;
+
+int arr[8];
+
+void DFS(int cnt) {
+	if (cnt == M)
+	{
+		for (int i = 0; i < M; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = 0; i < N; i++)
+	{
+		arr[cnt] = v[i];
+		DFS(cnt+1);
+	}
+}
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	cin >> N >> M;
+	for (int i = 0; i < N; i++)
+	{
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+	sort(v.begin(), v.end());
+	DFS(0);
+}
+
