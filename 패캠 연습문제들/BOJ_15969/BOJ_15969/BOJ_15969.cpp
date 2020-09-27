09eb2a0d4817831f7ce3aaccf248064963818c4c
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-27 16:06:22 +0900

    기초 연습문제 풀이

---
+++ 패캠 연습문제들/BOJ_15969/BOJ_15969/BOJ_15969.cpp
@@ -0,0 +1,28 @@
+﻿// BOJ_15969.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+vector<int> v;
+
+
+int main()
+{
+    int N;
+    cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+	sort(v.begin(), v.end());
+
+	cout << v.back() - v.front();
+}
+
