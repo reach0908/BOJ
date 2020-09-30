e5c55c8318359f40e867bbe5b570653ab696ac9e
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-09-30 22:19:26 +0900

    출력초과

---
+++ 00. 다시 풀어야 할 문제들/N과M(12)/N과M(12)/N과M(12).cpp
@@ -0,0 +1,59 @@
+﻿
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+const int MAX = 9;
+
+vector<int> v;
+int arr[MAX] = {-1};
+int N, M;
+int check[10001] = { 0 };
+
+void DFS(int cnt) {
+	if (cnt == M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = 0; i < N; i++)
+	{
+		if (arr[cnt]==v[i])
+		{
+			continue;
+		}
+		if (arr[cnt]<=v[i])
+		{
+			arr[cnt] = v[i];
+			DFS(cnt + 1);
+		}
+		
+	}
+}
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N >> M;
+	for (int i = 0; i < N; i++)
+	{
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+	sort(v.begin(), v.end());
+	DFS(0);
+
+	return 0;
+}
+
