e3157498e8cd8958527bcac94ac0eb6072a14f24
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-10-06 22:52:50 +0900

    BFS를 이용한 문제풀이

---
+++ 00. 다시 풀어야 할 문제들/연결요소의개수/연결요소의개수/연결요소의개수.cpp
@@ -0,0 +1,59 @@
+﻿
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+const int MAX = 1001;
+
+int N, M;
+vector<int> v[MAX];
+int visited[MAX] = { 0 };
+
+int cnt = 0;
+
+void BFS(int n){
+	if (visited[n]==1)
+	{
+		return;
+	}
+	queue<int> q;
+	q.push(n);
+	visited[n] = 1;
+	while (!q.empty())
+	{
+		int cur = q.front();
+		q.pop();
+		for (int i = 0; i < v[cur].size(); i++)
+		{
+			int next = v[cur][i];
+			if (visited[next] == 0) {
+				q.push(next);
+				visited[next] = 1;
+			}
+		}
+	}
+	cnt++;
+}
+
+int main()
+{
+    cin >> N >> M;
+	for (int i= 0;i < M;i++)
+	{
+		int a, b;
+		cin >> a >> b;
+		v[a].push_back(b);
+		v[b].push_back(a);
+	}
+	for (int i = 1; i <= N; i++)
+	{
+		BFS(i);
+	}
+	cout << cnt;
+	return 0;
+}
+
