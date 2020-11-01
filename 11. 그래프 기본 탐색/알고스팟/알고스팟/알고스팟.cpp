af4c4c277e11f2062522966a2f10fbd7cdea9bbe
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-11-01 11:22:37 +0900

    우선순위 큐와 BFS를 이용한 문제풀이

---
+++ 11. 그래프 기본 탐색/알고스팟/알고스팟/알고스팟.cpp
@@ -0,0 +1,64 @@
+﻿
+
+#include <iostream>
+#include <queue>
+
+const int MAX = 101;
+
+using namespace std;
+
+int N, M;
+int result;
+pair<int, int> moveDir[4] = { {0,1},{1,0},{0,-1},{-1,0} };
+
+string board[MAX];
+bool visited[MAX][MAX] = { false };
+
+int BFS(int startX, int startY) {
+
+	priority_queue<pair<int, pair<int, int>>,vector<pair<int, pair<int, int>>>,greater<pair<int, pair<int, int>>>> q;
+	q.push({ 0,{startY,startX} });
+	visited[startY][startX] = true;
+
+	while (!q.empty())
+	{
+		int curCnt = q.top().first;
+		int curY = q.top().second.first;
+		int curX = q.top().second.second;
+		q.pop();
+		if (curY==N-1&&curX==M-1)
+		{
+			return curCnt;
+		}
+		for (int i = 0; i < 4; i++)
+		{
+			int nextY = curY + moveDir[i].first;
+			int nextX = curX + moveDir[i].second;
+			if (0<=nextX&&nextX<M&&0<=nextY&&nextY<N)
+			{
+				if (board[nextY][nextX] == '0' && !visited[nextY][nextX])
+				{
+					q.push({ curCnt,{nextY,nextX} });
+					visited[nextY][nextX] = true;
+				}
+				if (board[nextY][nextX] == '1' && !visited[nextY][nextX])
+				{
+					q.push({ curCnt + 1,{nextY,nextX} });
+					visited[nextY][nextX] = true;
+				}
+			}
+		}
+	}
+
+}
+
+int main()
+{
+	cin >> M >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> board[i];
+	}
+	cout << BFS(0, 0);
+}
+
