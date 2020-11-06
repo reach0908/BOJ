e10f3c036c75f5a5d638368c3d3a5aea55e393bb
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-11-06 21:06:01 +0900

    NHN 모의고사 문제풀이

---
+++ NHN모의고사/모양판별하기/모양판별하기/모양판별하기.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int startX, startY, endX, endY;
+
+int main()
+{
+	cin >> startX >> startY >> endX >> endY;
+	int distX, distY;
+	distX = abs(endX - startX);
+	distY = abs(endY - startY);
+	if (distX==0&&distY==0)
+	{
+		cout << "DOT";
+	}
+	else {
+		if ((distX>0&&distY==0)||(distX==0&&distY>0))
+		{
+			cout << "SEGMENT";
+		}
+		if (distX>0&&distY>0) {
+			if (distX==distY)
+			{
+				cout << "SQUARE";
+			}
+			else {
+				cout << "RECTANGLE";
+			}
+		
+		}
+	}
+	return 0;
+}
+
