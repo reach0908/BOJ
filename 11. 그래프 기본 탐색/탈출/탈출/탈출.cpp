c7e5363b4f6f0073b0902b34616740856aa472de
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-11-06 21:06:38 +0900

    BFS를 이용한 문제풀이

---
+++ 11. 그래프 기본 탐색/탈출/탈출/탈출.cpp
@@ -0,0 +1,63 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 50;
+
+int R, C;
+char board[MAX][MAX];
+
+int startY, startX;
+int destY, destX;
+int waterY, waterX;
+
+int BFS() {
+
+
+
+
+	return -1;
+}
+
+int main()
+{
+	cin >> R >> C;  
+
+	for (int i = 0; i < R; i++)
+	{
+		string temp;
+		cin >> temp;
+		for (int j = 0; j < C; j++) {
+			board[i][j] = temp[j];
+			if (temp[j]=='D')
+			{
+				startX = j;
+				startY = i;
+			}
+			if (temp[j] == 'S')
+			{
+				destX = j;
+				destY = i;
+			}
+			if (temp[j] == '*')
+			{
+				waterX = j;
+				waterY = i;`
+			}
+
+		}
+	}
+	int result = BFS();
+	if (result==-1)
+	{
+		cout << "KAKTUS";
+	}
+	else {
+		cout << result;
+	}
+	return 0;
+
+}
+
