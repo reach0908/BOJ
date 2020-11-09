0f58d079a719119d0a756435eab475095eff33a2
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-11-09 19:15:17 +0900

    구현 연습문제 풀이

---
+++ 패캠 연습문제들/보너스문제/보너스문제/보너스문제.cpp
@@ -0,0 +1,42 @@
+﻿
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int MAX = 10000;
+
+int cache[MAX] = { 0 };
+
+int N;
+string s;
+
+int solve() {
+	int curBonus=0;
+	int score = 0;
+	for (int i = 0; i < N; i++)
+	{
+		if (s[i]=='X')
+		{
+			curBonus = 0;
+			continue;
+		}
+		else {
+			score += (i+ 1 + curBonus);
+			curBonus++;
+		}
+		
+	}
+	return score;
+}
+
+int main()
+{
+    cin >> N;
+    cin >> s;
+	cout << solve();
+	return 0;
+
+}
+
