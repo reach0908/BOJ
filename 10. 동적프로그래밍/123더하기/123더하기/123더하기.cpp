f13f782a64411cd82fd8b291f8cee3ec9e063a52
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-11-20 02:32:59 +0900

    점화식을 이용한 DP문제

---
+++ 10. 동적프로그래밍/123더하기/123더하기/123더하기.cpp
@@ -0,0 +1,29 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 11;
+
+int cache[MAX] = { 0 };
+
+int N,T;
+
+int main()
+{
+    cin >> T;
+	for (int i = 0; i < T; i++)
+	{
+		cin >> N;
+		cache[1] = 1;
+		cache[2] = 2;
+		cache[3] = 4;
+		for (int j = 4; j <=N; j++)
+		{
+			cache[j] = cache[j - 1] + cache[j - 2] + cache[j-3];
+		}
+		cout << cache[N] << endl;
+	}
+}
+
