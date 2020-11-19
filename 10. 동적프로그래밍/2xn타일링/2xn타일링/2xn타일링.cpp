dd019789089aa7b8c67a25d5f0f7f70c3d74863f
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-11-20 01:49:14 +0900

    DP문제, 점화식을 이용한 문제풀이

---
+++ 10. 동적프로그래밍/2xn타일링/2xn타일링/2xn타일링.cpp
@@ -0,0 +1,25 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 1001;
+
+int rect[MAX] = { 0 };
+
+int N;
+
+int main()
+{
+    cin >> N;
+	rect[1] = 1;
+	rect[2] = 2;
+	for (int i = 3; i <=N; i++)
+	{
+		rect[i] = (rect[i - 1] + rect[i - 2]) % 10007;
+	}
+	cout << rect[N]%10007;
+	return 0;
+}
+
