099b140b34dea647c1df86191c71459dcb4f13b8
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-11-20 03:21:35 +0900

    바텀업방식의 DP문제풀이 - 이전문제에서 최소최대만 바꿔주었다.

---
+++ 10. 동적프로그래밍/카드구매하기2/카드구매하기2/카드구매하기2.cpp
@@ -0,0 +1,37 @@
+﻿
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int MAX = 1001;
+
+int cache[MAX] = { 0 };
+
+int N;
+
+int price[MAX] = { 0 };
+
+int main()
+{
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> price[i];
+	}
+	for (int i = 1; i <= N; i++)
+	{
+		cache[i] = cache[i - 1] + price[1];
+		for (int j = 2; j <= N; j++)
+		{
+			if (i - j >= 0)
+			{
+				cache[i] = min(cache[i], cache[i - j] + price[j]);
+			}
+		}
+	}
+
+	cout << cache[N];
+	return 0;
+}
+
