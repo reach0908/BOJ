c0004283be77504fad248987747f35b0bbc10906
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-11-20 02:49:59 +0900

    배열을 Long으로 선언한 후 MOD 한번만 나누기

--- 10. 동적프로그래밍/123더하기3/123더하기3/123더하기3.cpp
+++ 10. 동적프로그래밍/123더하기3/123더하기3/123더하기3.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 const int MAX = 1000001;
-const int INF = 1000000009;
-int cache[MAX] = { 0 };
+const int MOD = 1000000009;
+long long cache[MAX] = { 0 };
 
 int N, T;
 
@@ -21,9 +21,9 @@ int main()
 		cache[3] = 4;
 		for (int j = 4; j <= N; j++)
 		{
-			cache[j] = ((cache[j - 1] + cache[j - 2])%INF + cache[j - 3])%INF;
+			cache[j] = (cache[j - 1] + cache[j - 2] + cache[j - 3])%MOD;
 		}
-		cout << cache[N] << endl;
+		cout << cache[N]%MOD << endl;
 	}
 	return 0;
 }
