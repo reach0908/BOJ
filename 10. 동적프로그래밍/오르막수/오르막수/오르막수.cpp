c0257a1bc033a867b970fcf5b8077cb58fc0760f
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-12-20 21:15:02 +0900

    동적프로그래밍 문제 풀이 _ 오르막수

---
+++ 10. 동적프로그래밍/오르막수/오르막수/오르막수.cpp
@@ -0,0 +1,46 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+const int MOD = 10007;
+const int MAX = 1001;
+
+
+int N;
+
+long long cache[MAX][10] = { 0 };
+
+int solve() {
+    for (int i = 2; i <=N; i++)
+    {
+        for (int j = 0; j <=9;j++)
+        {
+            for (int k = 0; k <= j; k++)
+            {
+                cache[i][j] = (cache[i][j] + cache[i - 1][j - k]) % MOD;
+            }
+        }
+    }
+    
+
+
+    long long result = 0LL;
+
+    for (int i = 0; i < 10; i++)
+    {
+        result += cache[N][i];
+    }
+    return result%MOD;
+}
+
+int main()
+{
+    cin >> N;
+    for (int i = 0; i < 10; i++)
+    {
+        cache[1][i] = 1;
+    }
+    cout << solve();
+
+}
