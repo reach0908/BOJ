80d1928f89811beb68ffc29d1e1902ea9ef12bb0
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-12-22 03:01:53 +0900

    이친수 첫번째 풀이

---
+++ 10. 동적프로그래밍/이친수/이친수/이친수.cpp
@@ -0,0 +1,27 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 91;
+
+int N;
+
+long long cache[MAX] = { 0 };
+
+int main()
+{
+
+    cache[1] = 1;
+    cache[2] = 1;
+   
+    cin >> N;
+    
+    for (int i = 3; i <= N; i++)
+    {
+        cache[i] = cache[i - 1] + cache[i - 2];
+    }
+    cout << cache[N];
+    return 0;
+}
