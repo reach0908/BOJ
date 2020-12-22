841ec7cb544ba9ae11f945339f524934f4e12591
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-12-23 06:22:06 +0900

    완전탐색과 비슷한 풀이

---
+++ 10. 동적프로그래밍/스티커/스티커/스티커.cpp
@@ -0,0 +1,45 @@
+﻿
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int T, n;
+
+const int MAX = 100001;
+int board[MAX][2] = { 0 };
+int cache[MAX][2] = { 0 };
+
+int solve(int startX,int startY, int curScore) {
+	cache[0][0] = cache[0][1] = 0;
+	cache[1][0] = board[1][0];
+	cache[1][1] = board[1][1];
+	for (int i = 2; i <= n; i++)
+	{
+		cache[i][0] = max(cache[i - 1][1], (i - 2 > 0) ? cache[i - 2][1] : 0) + board[i][0];
+		cache[i][1] = max(cache[i - 1][0], (i - 2 > 0) ? cache[i - 2][0] : 0) + board[i][1];
+	}
+	return max(cache[n][0], cache[n][1]);
+
+}
+
+int main()
+{
+    cin >> T;
+	for (int i = 0; i < T; i++)
+	{
+		cin >> n;
+		for (int j = 0; j < 2; j++)
+		{
+			for (int k = 1; k <= n; k++)
+			{
+				cin >> board[k][j];
+			}
+		}
+		
+		cout << solve(0, 0, 0) << endl;
+	}
+    return 0;
+}
+
