89f79297317248c046831f5c59e96814c7d028e9
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-12-26 06:32:23 +0900

    BOJ 14002번 풀이

---
+++ 10. 동적프로그래밍/가장긴증가하는부분수열4/가장긴증가하는부분수열4/가장긴증가하는부분수열4.cpp
@@ -0,0 +1,59 @@
+﻿
+
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+const int MAX = 1001;
+
+int N;
+
+int A[MAX] = { 0 };
+pair<int, vector<int>> cache[MAX];
+
+void solve() {
+	for (int i = 0; i < N; i++)
+	{
+		cache[i].first = 1;
+		cache[i].second.push_back(A[i]);
+		for (int j = 0; j < i; j++)
+		{
+			if (A[i]>A[j]&&cache[i].first<cache[j].first+1)
+			{
+				cache[i].first = cache[j].first+1;
+				cache[i].second = cache[j].second;
+				cache[i].second.push_back(A[i]);
+			}
+		}
+	}
+	int maxN = 0;
+	int resIndex = 0;
+	for (int i = 0; i < N; i++)
+	{
+		if (maxN<cache[i].first)
+		{
+			maxN = cache[i].first;
+			resIndex = i;
+		}
+	}
+	cout << cache[resIndex].first << endl;
+	for (int i = 0; i < cache[resIndex].second.size(); i++)
+	{
+		cout << cache[resIndex].second[i] << " ";
+	}
+
+}
+int main()
+{
+
+    cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> A[i];
+	}
+	solve();
+    return 0;
+}
+
