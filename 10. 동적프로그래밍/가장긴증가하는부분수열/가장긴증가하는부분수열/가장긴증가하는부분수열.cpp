051ba61153c8527ff8a3642ddd3924c2ef2a0539
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-12-26 06:21:14 +0900

    동적프로그래밍 문제풀이,

---
+++ 10. 동적프로그래밍/가장긴증가하는부분수열/가장긴증가하는부분수열/가장긴증가하는부분수열.cpp
@@ -0,0 +1,46 @@
+﻿
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int MAX = 1001;
+
+int N;
+
+int A[MAX] = { 0 };
+int cache[MAX] = { 0 };
+
+int solve() {
+	for (int i = 1; i <=N; i++)
+	{
+		cache[i] = 1;
+		for (int j = 1; j <=i; j++)
+		{
+			if (A[i]>A[j]&&cache[i]<cache[j]+1)
+			{
+				cache[i] = cache[j] + 1;
+			}
+		}
+	}
+	int maxN = 0;
+	for (int i = 1; i <= N; i++)
+	{
+		maxN = max(maxN, cache[i]);
+	}
+
+	return maxN;
+
+}
+
+int main()
+{
+    cin >> N;
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> A[i];
+	}
+	cout << solve();
+}
+
--- 10. 동적프로그래밍/스티커/스티커/스티커.cpp
+++ 10. 동적프로그래밍/스티커/스티커/스티커.cpp
@@ -15,6 +15,7 @@ int solve(int startX,int startY, int curScore) {
 	cache[0][0] = cache[0][1] = 0;
 	cache[1][0] = board[1][0];
 	cache[1][1] = board[1][1];
+
 	for (int i = 2; i <= n; i++)
 	{
 		cache[i][0] = max(cache[i - 1][1], (i - 2 > 0) ? cache[i - 2][1] : 0) + board[i][0];
