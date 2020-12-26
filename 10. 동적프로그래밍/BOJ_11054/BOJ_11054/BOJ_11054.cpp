61f4c4555bd3d13fbda7274d8f004c29a02a0b77
SSU\fopguy0908 <wlsghkim0908@gmail.com>
2020-12-27 07:09:33 +0900

    BOJ_11054 가장긴바이토닉수열 동적프로그래밍 문제풀이

---
+++ 10. 동적프로그래밍/BOJ_11054/BOJ_11054/BOJ_11054.cpp
@@ -0,0 +1,62 @@
+﻿
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int MAX = 1001;
+
+int A[MAX];
+int cacheP[MAX];
+int cacheM[MAX];
+
+int N;
+
+void solve() {
+	for (int i = 0; i < N; i++)
+	{
+		cacheP[i] = 1;
+		
+		for (int j = 0; j < i; j++)
+		{
+			if (A[i]>A[j]&&cacheP[i]<cacheP[j]+1)
+			{
+				cacheP[i] = cacheP[j] + 1;
+			}
+		}
+		cout << cacheP[i] << endl;
+	}
+	cout << "--------------" << endl;
+	for (int i = N-1; i >=0; i--)
+	{
+		cacheM[i] = 1;
+		for (int j = N - 1; j >i; j--)
+		{
+			if (A[i] > A[j] && cacheM[i] < cacheM[j] + 1)
+			{
+				cacheM[i] = cacheM[j] + 1;
+			}
+		}
+		cout << cacheM[i] << endl;
+	}
+
+	int res = 0;
+	for (int i = 0; i < N; i++)
+	{
+		res = max(res, cacheP[i]+cacheM[i]-1);
+	}
+	cout << res;
+
+}
+
+
+int main()
+{
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> A[i];
+	}
+	solve();
+}
