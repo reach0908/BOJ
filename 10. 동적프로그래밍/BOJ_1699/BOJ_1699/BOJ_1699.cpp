add680252de0ef94503680cf857b05176ff9d7c6
fopguy0908 <wlsghkim0908@gmail.com>
2020-12-30 09:10:11 +0900

    BOJ_1699번 제곱수의 합 문제 풀이

---
+++ 10. 동적프로그래밍/BOJ_1699/BOJ_1699/BOJ_1699.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+const int MAX = 100001;
+
+int N;
+int cache[MAX] = { 0 };
+void solve() {
+	for (int i = 1; i <=N; i++)
+	{
+		for (int j = 1; j * j <= i; j++)
+		{
+			if (cache[i]>cache[i-j*j]+1||cache[i]==0)
+			{
+				cache[i] = cache[i - j * j] + 1;
+			}
+		}
+	}
+	cout << cache[N];
+}
+
+int main()
+{
+    cin >> N;
+	cache[0] = 0;
+    solve();
+}
+
