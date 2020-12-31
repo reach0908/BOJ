1e77e0ef47c7dc570b03f2d92368a80365b32999
reach0908 <wlsghkim0908@gmail.com>
2021-01-01 05:22:24 +0900

    BOJ_2529 permutation을 이용한 문제풀이

--- 02. 브루트포스/BOJ_2529/BOJ_2529/main.cpp
+++ 02. 브루트포스/BOJ_2529/BOJ_2529/main.cpp
@@ -9,65 +9,56 @@
 #include <vector>
 #include <string>
 #include <algorithm>
-const int MAX = 10;
 
 using namespace std;
 
-int K;
-
-string ansMIN = "999999999";
-string ansMAX = "0";
+vector<int> maxNum;
+vector<int> minNum;
 
 vector<char> b;
-bool visited[MAX] = {false};
 
-void solve(int numIndex,int bIndex, string num){
-    if(numIndex>K-1&&bIndex>K-1){
-        if(stoi(ansMIN)>stoi(num)){
-            ansMIN=num;
-        }
-        if(stoi(ansMAX)<stoi(num)){
-            ansMAX = num;
-        }
-        return;
-    }
-    
-    for (int i=0; i<MAX; i++) {
-        if(num==""){
-            visited[i] = true;
-            solve(numIndex, bIndex, num+to_string(i));
-            visited[i]=false;
-        }else{
-            if(!visited[i]){
-                if (b[bIndex]=='>') {
-                    if (num[numIndex]-'0'>i) {
-                        visited[i] = true;
-                        solve(numIndex+1, bIndex+1, num+to_string(i));
-                        visited[i]=false;
-                    }
-                }else{
-                    if (num[numIndex]-'0'<i) {
-                        visited[i] = true;
-                        solve(numIndex+1, bIndex+1, num+to_string(i));
-                        visited[i]=false;
-                    }
-                }
-            }
+int K;
+bool check(vector<int> &v){
+    for (int i=0; i<b.size(); i++) {
+        if(b[i]=='<'&& v[i]>v[i+1]){
+            return false;
+        }else if(b[i]=='>'&&v[i]<v[i+1]){
+            return false;
         }
     }
-    
-    
+    return true;
+}
+
+void solve(){
+    for(int i=9;i>9-(K+1);i--){
+        maxNum.push_back(i);
+    }
+    while (!check(maxNum)) {
+        prev_permutation(maxNum.begin(), maxNum.end());
+    }
+    for(int i=0;i<K+1;i++){
+        minNum.push_back(i);
+    }
+    while(!check(minNum)){
+        next_permutation(minNum.begin(), minNum.end());
+    }
 }
 
 int main() {
+
     cin >> K;
-    for (int i=0; i<K; i++) {
+    for(int i=0;i<K;i++){
         char temp;
         cin >> temp;
         b.push_back(temp);
     }
-    solve(0,0,"");
-    cout << ansMAX << endl;
-    cout << ansMIN << endl;
+    solve();
+    for (int i=0; i<maxNum.size(); i++) {
+        cout << maxNum[i];
+    }
+    cout << endl;
+    for(int i=0; i<minNum.size();i++){
+        cout << minNum[i];
+    }
     return 0;
 }
