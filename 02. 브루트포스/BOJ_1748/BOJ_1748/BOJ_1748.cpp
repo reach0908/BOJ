142902c7f96b6d592b5357bff75c7114404248ee
fopguy0908 <wlsghkim0908@gmail.com>
2020-12-31 11:06:35 +0900

    BOJ_1748번 수 이어쓰기 1 문제풀이

---
+++ 02. 브루트포스/BOJ_1748/BOJ_1748/BOJ_1748.cpp
@@ -0,0 +1,25 @@
+﻿
+#include <iostream>
+#include<string>
+#include<math.h>
+
+using namespace std;
+string N;
+int main()
+{
+	cin >> N;
+	long ans=0;
+	int len = N.length();
+	if (len == 1) {
+		cout << N;
+		return 0;
+	}
+	for (int i = 1; i < len; i++)
+	{
+		ans += i * (9 * pow(10, i - 1));
+	}
+	ans += len * (stoi(N) - pow(10, len - 1) + 1);
+	cout << ans;
+}
+
+
