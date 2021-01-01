806ca6f7015bcaee9dab482a94074ad6adcc40f6
fopguy0908 <wlsghkim0908@gmail.com>
2021-01-02 02:49:39 +0900

    BOJ_1339번 단어수학 문제풀이.

---
+++ 02. 브루트포스/BOJ_1339/BOJ_1339/BOJ_1339.cpp
@@ -0,0 +1,59 @@
+﻿
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+vector<string> word;
+int alphabet[26] = {0};
+int N;
+
+bool comp(int a, int b) {
+	return a > b;
+}
+
+void solve() {
+	sort(alphabet, alphabet+26,comp);
+	int temp = 9;
+	int total = 0;
+	for (int i = 0; i < 26; i++)
+	{
+		if (alphabet[i] != 0) {
+			total += temp * alphabet[i];
+			temp--;
+		}
+	}
+	cout << total;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+    cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		string temp;
+		cin >> temp;
+		word.push_back(temp);
+	}
+	
+	for (int i = 0; i < word.size(); i++)
+	{
+		for (int j = 0; j < word[i].length(); j++)
+		{
+			alphabet[word[i][j] - 'A'] += pow(10, word[i].length() - (j+1));
+		}
+	}
+	solve();
+
+	return 0;
+}
+
