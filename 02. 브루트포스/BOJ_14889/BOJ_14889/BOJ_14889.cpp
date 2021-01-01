45b9785abd9a4af695277be002804679b591cb96
fopguy0908 <wlsghkim0908@gmail.com>
2021-01-02 08:33:10 +0900

    BOJ_14889번 스타트와 링크 문제 풀이

---
+++ 02. 브루트포스/BOJ_14889/BOJ_14889/BOJ_14889.cpp
@@ -0,0 +1,65 @@
+﻿
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+const int MAX = 21;
+const int INF = 999999999;
+int N;
+vector<int> team;
+int players[MAX][MAX] = {0};
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		if (i < N / 2) {
+			team.push_back(0);
+		}
+		else {
+			team.push_back(1);
+		}
+		for (int j = 0; j < N; j++)
+		{
+			cin >> players[i][j];
+		}
+	}
+	int result = INF;
+	do
+	{
+		
+		int tempA = 0;
+		int tempB = 0;
+		int temp = 0;
+
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < N; j++)
+			{
+				
+					if (team[i] == 0 && team[j] == 0)
+					{
+						tempA += players[i][j];
+						
+					}
+					else if (team[i] == 1 && team[j] == 1) {
+						tempB += players[i][j];
+						
+					}
+				
+			}
+		}
+		
+		temp = abs(tempA - tempB);
+		result = min(result, temp);
+	} while (next_permutation(team.begin(),team.end()));
+	cout << result;
+
+	return 0;
+}
+
