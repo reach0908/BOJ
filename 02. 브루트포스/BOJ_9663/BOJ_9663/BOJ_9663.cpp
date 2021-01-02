2e01300a0848de09d6bbcb70daadfde7abfb820f
fopguy0908 <wlsghkim0908@gmail.com>
2021-01-02 12:58:22 +0900

    BOJ_9663번 N-queen 문제 풀이

---
+++ 02. 브루트포스/BOJ_9663/BOJ_9663/BOJ_9663.cpp
@@ -0,0 +1,45 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 16;
+
+int N;
+int result=0;
+
+int col[MAX] = { 0 };
+
+bool check(int level) {
+	for (int i = 0; i < level; i++)
+	{
+		if (col[i] == col[level] || abs(col[level] - col[i]) == level - i) {
+			return false;
+		}
+	}
+	return true;
+}
+
+
+void nqueen(int x) {
+	if (x == N) {
+		result++;
+	}
+	else {
+		for (int i = 0; i < N; i++)
+		{
+			col[x] = i;
+			if (check(x))
+			{
+				nqueen(x + 1);
+			}
+		}
+	}
+}
+int main()
+{
+    cin >> N;
+	nqueen(0);
+	cout << result;
+}
