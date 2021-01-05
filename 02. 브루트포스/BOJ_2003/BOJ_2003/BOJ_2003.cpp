2003c1688b45cd7ed547dfc387647687e6ec2adf
fopguy0908 <wlsghkim0908@gmail.com>
2021-01-05 10:07:31 +0900

    BOJ_2003번 수들의 합2 문제풀이

---
+++ 02. 브루트포스/BOJ_2003/BOJ_2003/BOJ_2003.cpp
@@ -0,0 +1,37 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 10000;
+
+int N, M;
+int A[MAX];
+int cnt=0;
+
+int main()
+{
+    cin >> N >> M;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> A[i];
+	}
+	
+	for (int i = 0; i < N; i++)
+	{
+		int sum = A[i];
+		if (sum == M) {
+			cnt++;
+		}
+		for (int j = i+1; j < N; j++)
+		{
+			sum += A[j];
+			if (sum == M) {
+				cnt++;
+			}
+		}
+	}
+	cout << cnt;
+}
+
