404e5bf80546cc3e1bcde27223ccacf6f88c9e2c
fopguy0908 <wlsghkim0908@gmail.com>
2021-01-11 05:23:15 +0900

    BOJ_7453번 합이 0인 네 정수 문제풀이 / lower upperbound 이용

---
+++ 02. 브루트포스/BOJ_7453/BOJ_7453/BOJ_7453.cpp
@@ -0,0 +1,47 @@
+﻿
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+const int MAX = 4000;
+int n;
+
+int arr[4][MAX];
+vector<int> l;
+vector<int> r;
+int ans=0;
+int main()
+{
+	
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			cin >> arr[j][i];
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j= 0; j < n; j++)
+		{
+			l.push_back(arr[0][i] + arr[1][j]);
+			r.push_back(arr[2][i] + arr[3][j]);
+		}
+	}
+	sort(l.begin(), l.end());
+	sort(r.begin(), r.end());
+	for (int i = 0; i < l.size(); i++)
+	{
+		int low = lower_bound(r.begin(), r.end(), 0 - l[i]) - r.begin();
+		int high = upper_bound(r.begin(), r.end(), 0 - l[i]) - r.begin();
+
+		ans += high - low;
+	}
+	cout << ans;
+}
+
