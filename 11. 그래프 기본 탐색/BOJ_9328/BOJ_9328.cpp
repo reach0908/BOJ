f9777c4c9cb8d70597a605ddf25641329765d741
fopguy0908 <wlsghkim0908@gmail.com>
2021-01-16 03:50:12 +0900

    수정

--- 11. 그래프 기본 탐색/BOJ_9328/BOJ_9328.cpp
+++ 11. 그래프 기본 탐색/BOJ_9328/BOJ_9328.cpp
@@ -1,114 +1,107 @@
 #include <iostream>
 #include <cstring>
 #include <queue>
+
 using namespace std;
 
 const int MAX = 102;
-
-int h,w;
-
-char mp[MAX][MAX];
+int t,h, w;
+string mp[MAX];
 bool visited[MAX][MAX];
 
-typedef struct options
+pair<int, int> dir[4] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+int result = 0;
+void BFS(string key)
 {
-    int y;
-    int x;
-}options;
-
-pair<int,int> dir[4]={{-1,0},{1,0},{0,-1},{0,1}};
-
-int BFS(string key)
-{
-    int result=0;
-    queue<options> q;
-    options op = {0, 0};
+    
+    queue<pair<int, int>> q;
+    q.push({0, 0});
     visited[0][0] = true;
-    q.push(op);
     while (!q.empty())
     {
-        int curX = q.front().x;
-        int curY = q.front().y;
+        int curY = q.front().first;
+        int curX = q.front().second;
+        visited[curY][curX] = true;
         q.pop();
         for (int i = 0; i < 4; i++)
         {
             int nextY = curY + dir[i].first;
             int nextX = curX + dir[i].second;
-            if (0 <= nextX && nextX <= w + 1 && 0 <= nextY && nextY <= h + 1)
+            if (0 <= nextX && nextX <= w + 1 && 0 <= nextY && nextY <= h + 1 && !visited[nextY][nextX])
             {
-                if (!visited[nextY][nextX]&&mp[nextY][nextX]!='*')
+                if (mp[nextY][nextX] == '*')
                 {
-                    if (mp[nextY][nextX] == '.')
+                    continue;
+                }
+                if (mp[nextY][nextX] == '.')
+                {
+                    q.push({nextY, nextX});
+                }
+                if ('A' <= mp[nextY][nextX] && mp[nextY][nextX] <= 'Z')
+                {
+                    if (key.find(char(mp[nextY][nextX] + 32)) != string::npos)
                     {
+                        mp[nextY][nextX] = '.';
                         q.push({nextY, nextX});
-                        visited[nextY][nextX] = true;
                     }
-                    if ('A' <= mp[nextY][nextX]&& mp[nextY][nextX] <= 'Z')
+                }
+                if ('a' <= mp[nextY][nextX] && mp[nextY][nextX] <= 'z')
+                {
+                    if (key.find(mp[nextY][nextX]) == string::npos)
                     {
-                        if (key.find(mp[nextY][nextX]+32))
+                        key += mp[nextY][nextX];
+                        mp[nextY][nextX] = '.';
+                        memset(visited, false, sizeof(visited));
+                        while (!q.empty())
                         {
-                            q.push({nextY, nextX});
-                            visited[nextY][nextX] = true;
+                            q.pop();
                         }
-                    }
-                    if ('a' <= mp[nextY][nextX]&&mp[nextY][nextX] <= 'z')
-                    {
-                        string nextKey = key + mp[nextY][nextX];
                         q.push({nextY, nextX});
-                        visited[nextY][nextX] = true;
                     }
-                    if (mp[nextX][nextY]=='$')
-                    {
-                        q.push({nextY,nextX});
-                        visited[nextY][nextX]=true;
-                        result++;
-                    }
+                }
+                if (mp[nextY][nextX] == '$')
+                {
+                    result++;
+                    mp[nextY][nextX] = '.';
+                    q.push({nextY, nextX});
                 }
             }
         }
     }
-    return result;
 }
 
-int main(int argc, char const *argv[])
+int main()
 {
-    int t;
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
     cin >> t;
-    while (t>0)
+    while (t > 0)
     {
         cin >> h >> w;
-        string temp;
         //전체 초기화
-        for (int i = 0; i <= MAX; i++)
-        {
-            memset(mp[i], '*', sizeof(mp[i]));
-            memset(visited[i],false,sizeof(visited[i]));
-        }
+        memset(visited, false, sizeof(visited));
+        for (int i = 0; i < MAX; i++)
+            mp[i].clear();
         //상근이가 갈수있는 곳 초기화
-        for (int i = 0; i <= h + 1; i++)
-        {
-            for (int j = 0; j <= w + 1; j++)
-            {
-                mp[i][j] = '.';
-            }
-        }
+        for (int i = 0; i < w + 2; i++)
+            mp[0] += '.';
         for (int i = 1; i <= h; i++)
         {
+            string temp;
             cin >> temp;
-            for (int j = 1; j <= w; j++)
-            {
-                mp[i][j] = temp[j-1];
-            }
+            mp[i] += '.';
+            mp[i] += temp;
+            mp[i] += '.';
         }
-        string key;
-        cin >> key;
-        if(key=="0"){
-            key = "";
-        }
-        cout << BFS(key)<<endl;
+        for (int i = 0; i < w + 2; i++)
+            mp[h + 1] += '.';
+        string alphabet;
+        cin >> alphabet;
+        result=0;
+        BFS(alphabet);
+        cout << result << endl;
         t--;
     }
-    
-    
     return 0;
 }
