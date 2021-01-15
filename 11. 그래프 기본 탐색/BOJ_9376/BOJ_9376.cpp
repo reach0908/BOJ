5647c78b6c755aa5f862d1f5498ee43fbce0147a
fopguy0908 <wlsghkim0908@gmail.com>
2021-01-16 04:52:33 +0900

    키포인트 추가

--- 11. 그래프 기본 탐색/BOJ_9376/BOJ_9376.cpp
+++ 11. 그래프 기본 탐색/BOJ_9376/BOJ_9376.cpp
@@ -1,70 +1,137 @@
 #include <iostream>
 #include <cstring>
 #include <queue>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 const int MAX = 102;
 const int INF = 1e9;
-int board[MAX][MAX];
-bool visited[MAX][MAX]={false};
 
-typedef struct options
-{
-    int doorCnt;
-    int pCnt;
-    int y;
-    int x;
+string board[MAX];
+
+typedef struct options{
+    int y,x,val;
 }options;
+struct cmp
+{
+    bool operator()(options a,options b){
+        return a.val > b.val;
+    }
+};
 
 int t,h,w;
 pair<int,int> moveDir[4] = {{1,0},{-1,0},{0,1},{0,-1}};
+int res[3][MAX][MAX];
+bool visited[MAX][MAX];
 
-int BFS(){
-   
+vector<options> point;
+
+void BFS(int y, int x, int num)
+{
+    priority_queue<options, vector<options>, cmp> q;
+    
+    q.push({y,x,0});
+    visited[y][x] = true;
+    res[num][y][x] =0;
+
+    while (!q.empty())
+    {
+        options op = q.top();
+        q.pop();
+        for (int i = 0; i < 4; i++)
+        {
+            int nextY = op.y + moveDir[i].first;
+            int nextX = op.x + moveDir[i].second;
+            int curval = op.val;
+            if(nextY<0||nextY>h+1||nextX<0||nextX>w+1){
+                continue;
+            }
+            if(visited[nextY][nextX]||board[nextY][nextX]=='*'){
+                continue;
+            }
+            if(board[nextY][nextX]=='#'){
+                curval++;
+            }
+            q.push({nextY,nextX,curval});
+            visited[nextY][nextX]=true;
+            res[num][nextY][nextX]=curval;
+        }
+    }
 }
 
-int main(int argc, char const *argv[])
+int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    
     cin >> t;
-    for (int i = 0; i < t; i++)
+    while (t>0)
     {
-        cin >> h >> w;
-        for (int j = 0; j < MAX; j++)
+        memset(visited,false,sizeof(visited));
+        memset(res,0,sizeof(res));
+        point.clear();
+        point.push_back({0,0}); 
+
+        for (int i = 0; i < MAX; i++)
         {
-            memset(board[j],-1,sizeof(board[j]));
-            memset(visited[j],false,sizeof(visited[j]));
+            board[i].clear();
         }
-        
-        for (int j = 1; j <= h; j++)
+        cin >> h >> w;
+        for (int i = 0; i < w+2; i++)
         {
-            string temp;
-            cin >> temp;
-            for (int k = 1; k <= w; k++)
+            board[0] += '.';
+            board[h+1] += '.';
+        }
+        for (int i = 1; i <= h; i++)
+        {
+            cin >> board[i];
+            board[i] = '.' + board[i];
+            for (int j = 1; j <=w; j++)
             {
-                if (temp[k] == '*')
-                {
-                    board[j][k] = 0;
-                }
-                else if (temp[k] == '.')
-                {
-                    board[j][k] = 1;
-                }
-                else if (temp[k] == '$')
-                {
-                    board[j][k] = 2;
-                }
-                else if (temp[k] == '#')
-                {
-                    board[j][k] = 3;
+                if(board[i][j]=='$'){
+                    point.push_back({i,j});
                 }
             }
+            board[i] += '.';
         }
-        cout << BFS() << endl;
+        for (int i = 0; i <3; i++)
+        {
+            for (int j = 0; j <=h; j++)
+            {
+                for (int k = 0; k <= w; k++)
+                {
+                    res[i][j][k]=INF;
+                }
+            }
+            
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            BFS(point[i].y,point[i].x,i);
+        }
+        
+        long long ans = INF;
+        for (int i = 1; i <= h; i++)
+        {
+            for (int j = 1; j <=w; j++)
+            {
+                if(board[i][j]=='*'){
+                    continue;
+                }
+                long long tmp =0;
+                for (int k = 0; k < 3; k++)
+                {
+                    tmp += res[k][i][j];
+                }
+                if(board[i][j]=='#'){
+                    tmp-=2;
+                }
+                ans = min(ans,tmp);
+            }
+        }
+        cout << ans << endl;
+        t--;
     }
-
     return 0;
 }
