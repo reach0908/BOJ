0945c973e49bcfcedafa332ae01a64177e740680
fopguy0908 <wlsghkim0908@gmail.com>
2021-04-13 23:03:57 +0900

    BOJ_2292번 문제풀이 / 수학

---
+++ 01. 수학/BOJ_2292/BOJ_2292/BOJ_2292.cpp
@@ -0,0 +1,25 @@
+﻿
+
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+
+int solve(int num) {
+    int cnt = 0;
+    for (int i = 1; i < num; i+=(6*cnt))
+    {
+        cnt++;
+    }
+    return cnt+1;
+}
+
+int main()
+{
+    cin >> N;
+    cout << solve(N) << endl;
+    return 0;
+    
+}
