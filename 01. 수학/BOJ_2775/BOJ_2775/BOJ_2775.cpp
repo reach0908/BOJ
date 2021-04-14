9952b85e61e8e36581bd5c037dc165f5696cb707
fopguy0908 <wlsghkim0908@gmail.com>
2021-04-15 00:11:08 +0900

    BOJ_2775번 문제풀이 / 수학

---
+++ 01. 수학/BOJ_2775/BOJ_2775/BOJ_2775.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 15;
+
+int K, N,T;
+int res;
+
+int arr[MAX][MAX];
+
+void solve() {
+	for (int i = 1; i < MAX; i++)
+	{
+		arr[0][i] = i;
+	}
+	for (int i = 1; i < MAX; i++)
+	{
+		for (int j = 1; j < MAX; j++)
+		{
+			int cnt = 0;
+			
+				for (int x = 1; x <=j; x++)
+				{
+					cnt += arr[i-1][x];
+				}
+			
+			arr[i][j] = cnt;
+		}
+	}
+}
+
+int main()
+{
+	cin >> T;
+	solve();
+	while (T--)
+	{
+		cin >> K >> N;
+		cout << arr[K][N] << endl;
+	}
+}
+
