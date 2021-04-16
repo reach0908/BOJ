4649489b2331b3ce0f6853be371e840a7b83328b
fopguy0908 <wlsghkim0908@gmail.com>
2021-04-17 00:08:10 +0900

    BOJ_2798번 문제풀이 / 브루트포스

---
+++ 02. 브루트포스/BOJ_2798/BOJ_2798/BOJ_2798.cpp
@@ -0,0 +1,45 @@
+﻿
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+vector<int> v;
+vector<int> arr;
+int N, M;
+int res = -1;
+
+int main()
+{
+    cin >> N >> M;
+	for (int i = 0; i < N; i++)
+	{
+		int temp;
+		cin >> temp;
+		arr.push_back(temp);
+	}
+	for (int i = 0; i < N-3; i++)
+	{
+		v.push_back(0);
+	}for (int i = 0; i < 3; i++)
+	{
+		v.push_back(1);
+	}
+	do
+	{
+		int sum = 0;
+		for (int i = 0; i < N; i++)
+		{
+			if (v[i] == 1) {
+				sum += arr[i];
+			}
+		}
+		if (sum <= M) {
+			res = max(res, sum);
+		}
+	} while (next_permutation(v.begin(),v.end()));
+	cout << res;
+}
+
