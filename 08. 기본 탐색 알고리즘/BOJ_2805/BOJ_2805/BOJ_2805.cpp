44124bcf1d21353423982df4b4e1faa0a6d9403f
fopguy0908 <wlsghkim0908@gmail.com>
2021-04-18 23:35:26 +0900

    BOJ_2805번 문제풀이 / 이진 탐색

---
+++ 08. 기본 탐색 알고리즘/BOJ_2805/BOJ_2805/BOJ_2805.cpp
@@ -0,0 +1,55 @@
+﻿
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+vector<long long> v;
+long long L = 1;
+long long R = 0;
+long long result = 0;
+long long isMatch(long long n) {
+	long long sum = 0;
+	for (int i = 0; i < N; i++)
+	{
+		if (v[i] - n>0) {
+			sum += v[i] - n;
+		}
+	}
+	if (sum >= M) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+int main()
+{
+	cin >> N >> M;
+	for (int i = 0; i < N; i++)
+	{
+		long long temp;
+		cin >> temp;
+		R = max(R, temp);
+		v.push_back(temp);
+	}
+	sort(v.begin(), v.end());
+	while (L <= R)
+	{
+		long long mid = (L + R) / 2;
+		if (isMatch(mid)) {
+			result = max(result, mid);
+			L = mid + 1;
+		}
+		else {
+			R = mid - 1;
+		}
+	}
+	cout << result << endl;
+	return 0;
+}
